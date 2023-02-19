d921f895b8815daaa6520d3a60554dd0823335d1
alina <a.vdovichenko@g.nsu.ru>
2023-02-19 23:35:41 +0700

    test

--- main.c
+++ main.c
@@ -5,20 +5,21 @@
 int N=10000000;
 
 int main() {
-    clock_t a=clock();
+
     float* arr = (float*)malloc((N+1)*sizeof(float));
     float Sum=0;
-
+    clock_t a=clock();
 #pragma acc kernels
     for (int i = 0; i < N; i++) {
         arr[i] = (float)sin(2 * M_PI * i / N);
     }
+    clock_t b=clock();
 #pragma acc kernels
     for (int i = 0; i < N; i++) {
         Sum+=arr[i];
     }
     printf("%.25f\n", Sum);
-    clock_t b=clock();
+
     double d=(double)(b-a)/CLOCKS_PER_SEC;
     printf("%.25f", d);
     return 0;
