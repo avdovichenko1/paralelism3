cf33990b4e1ab991f6df956320ba4582855e96cf
avdovichenko1 <92294006+avdovichenko1@users.noreply.github.com>
2023-03-10 10:14:07 +0700

    Update example.c

--- example.c
+++ example.c
@@ -1,67 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <malloc.h>
 #include <math.h>
+#include <time.h>
 #include <cuda_runtime.h>
-#include "cublas_v2.h"
-#define M 6
-#define N 5
-#define IDX2F(i,j,ld) ((((j)-1)*(ld))+((i)-1))
+#include <cublas_v2.h>
 
-static __inline__ void modify (cublasHandle_t handle, float *m, int ldm, int n, int p, int q, float alpha, float beta){
-    cublasSscal (handle, n-q+1, &alpha, &m[IDX2F(p,q,ldm)], ldm);
-    cublasSscal (handle, ldm-p+1, &beta, &m[IDX2F(p,q,ldm)], 1);
-}
+#define N 10000000
+#define ALPHA 1.0f
 
-int main (void){
-    cudaError_t cudaStat;
-    cublasStatus_t stat;
+int main() {
+    float *arr, *d_arr;
+    float Sum = 0;
+    clock_t a = clock();
+
+    // Allocate memory for the arrays
+    arr = (float*)malloc(N * sizeof(float));
+    cudaMalloc(&d_arr, N * sizeof(float));
+
+    // Initialize the array on the CPU
+    for (int i = 0; i < N; i++)
+        arr[i] = sinf(2 * M_PI * i / N);
+
+    // Copy the array to the GPU
+    cudaMemcpy(d_arr, arr, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    // Create a handle for the cuBLAS library
     cublasHandle_t handle;
-    int i, j;
-    float* devPtrA;
-    float* a = 0;
-    a = (float *)malloc (M * N * sizeof (*a));
-    if (!a) {
-        printf ("host memory allocation failed");
-        return EXIT_FAILURE;
-    }
-    for (j = 1; j <= N; j++) {
-        for (i = 1; i <= M; i++) {
-            a[IDX2F(i,j,M)] = (float)((i-1) * N + j);
-        }
-    }
-    cudaStat = cudaMalloc ((void**)&devPtrA, M*N*sizeof(*a));
-    if (cudaStat != cudaSuccess) {
-        printf ("device memory allocation failed");
-        return EXIT_FAILURE;
-    }
-    stat = cublasCreate(&handle);
-    if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("CUBLAS initialization failed\n");
-        return EXIT_FAILURE;
-    }
-    stat = cublasSetMatrix (M, N, sizeof(*a), a, M, devPtrA, M);
-    if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("data download failed");
-        cudaFree (devPtrA);
-        cublasDestroy(handle);
-        return EXIT_FAILURE;
-    }
-    modify (handle, devPtrA, M, N, 2, 3, 16.0f, 12.0f);
-    stat = cublasGetMatrix (M, N, sizeof(*a), devPtrA, M, a, M);
-    if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("data upload failed");
-        cudaFree (devPtrA);
-        cublasDestroy(handle);
-        return EXIT_FAILURE;
-    }
-    cudaFree (devPtrA);
+    cublasCreate(&handle);
+
+    // Perform the vector operation
+    cublasSaxpy(handle, N, &ALPHA, d_arr, 1, d_arr, 1);
+
+    // Copy the result back to the CPU
+    cudaMemcpy(arr, d_arr, N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    // Wait for all CUDA work to complete
+    cudaDeviceSynchronize();
+
+    // Sum the array on the CPU
+    for (int i = 0; i < N; i++)
+        Sum += arr[i];
+
+    // Print the result
+    printf("%.25f\n", Sum);
+
+    // Destroy the cuBLAS handle
     cublasDestroy(handle);
-    for (j = 1; j <= N; j++) {
-        for (i = 1; i <= M; i++) {
-            printf ("%7.0f", a[IDX2F(i,j,M)]);
-        }
-        printf ("\n");
-    }
-    free(a);
-    return EXIT_SUCCESS;
+
+    // Free the memory
+    free(arr);
+    cudaFree(d_arr);
+
+    clock_t b = clock();
+    double d = (double)(b-a) / CLOCKS_PER_SEC;
+    printf("%.25f\n", d);
+
+    return 0;
 }
+
