bc7c9a84e8575f7c324769ab1c550212ede59c04
avdovichenko1 <92294006+avdovichenko1@users.noreply.github.com>
2023-03-27 12:25:08 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,76 +1,95 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <math.h> 
-#include <cuda_runtime.h> 
-#include "cublas_v2.h" 
+#include <time.h> 
  
-#define M 1024 
-#define TOL 0.000001
-#define ITER_MAX 1000000 
+int main(int argc, char* argv[]) { 
+     
+    double time_spent1 = 0.0; 
  
-int main() { 
+    clock_t begin1 = clock();  
+     
+    // Check if enough arguments are provided 
+    if (argc < 4) { 
+        printf("Usage: ./program_name Matrix accuracy iterations\n"); 
+        return 1; 
+    } 
+ 
+    // Convert command line arguments to integers 
+    int Matrix = atoi(argv[1]); 
+    double accuracy = atof(argv[2]); 
+    int iterations = atoi(argv[3]); 
+     
     // Allocate 2D arrays on host memory 
-    double *arr = (double *)malloc(M * M * sizeof(double)); 
-    double *arrNew = (double *)malloc(M * M * sizeof(double)); 
+    double* arr = (double*)malloc(Matrix * Matrix * sizeof(double)); 
+    double* array_new = (double*)malloc(Matrix * Matrix * sizeof(double)); 
     // Initialize arrays to zero 
-    for (int i = 0; i < M * M; i++) { 
+    for (int i = 0; i < Matrix * Matrix; i++) { 
         arr[i] = 0; 
-        arrNew[i] = 0; 
+        array_new[i] = 0; 
     } 
     // Set boundary conditions 
-    arr[0*M+0] = 10; 
-    arr[0*M+M-1] = 20; 
-    arr[(M-1)*M+0] = 30; 
-    arr[(M-1)*M+M-1] = 20; 
+    arr[0 * Matrix + 0] = 10; 
+    arr[0 * Matrix + Matrix - 1] = 20; 
+    arr[(Matrix - 1) * Matrix + 0] = 30; 
+    arr[(Matrix - 1) * Matrix + Matrix - 1] = 20; 
  
     // Main loop 
-    double err = TOL + 1; 
+    double err = accuracy + 1; 
     int iter = 0; 
  
-    #pragma acc data copy(arr[0:M*M], arrNew[0:M*M]) 
+#pragma acc data copy(arr[0:Matrix*Matrix], array_new[0:Matrix*Matrix]) 
     { 
-        #pragma acc kernels loop independent 
-        for(int j = 1; j < M; j++){ 
-            arr[0*M+j] = (arr[0*M+M-1] - arr[0*M+0])/(M-1) + arr[0*M+j-1];   //top 
-            arr[(M-1)*M+j] = (arr[(M-1)*M+M-1] - arr[(M-1)*M+0])/(M-1) + arr[(M-1)*M+j-1]; //bottom 
-            arr[j*M+0] = (arr[(M-1)*M+0] - arr[0*M+0])/(M-1) + arr[(j-1)*M+0]; //left 
-            arr[j*M+M-1] = (arr[(M-1)*M+M-1] - arr[0*M+M-1])/(M-1) + arr[(j-1)*M+M-1]; //right 
+#pragma acc kernels loop independent 
+        for (int j = 1; j < Matrix; j++) { 
+            arr[0 * Matrix + j] = (arr[0 * Matrix + Matrix - 1] - arr[0 * Matrix + 0]) / (Matrix - 1) + arr[0 * Matrix + j - 1];   //top 
+            arr[(Matrix - 1) * Matrix + j] = (arr[(Matrix - 1) * Matrix + Matrix - 1] - arr[(Matrix - 1) * Matrix + 0]) / (Matrix - 1) + arr[(Matrix - 1) * Matrix + j - 1]; //bottom 
+            arr[j * Matrix + 0] = (arr[(Matrix - 1) * Matrix + 0] - arr[0 * Matrix + 0]) / (Matrix - 1) + arr[(j - 1) * Matrix + 0]; //left 
+            arr[j * Matrix + Matrix - 1] = (arr[(Matrix - 1) * Matrix + Matrix - 1] - arr[0 * Matrix + Matrix - 1]) / (Matrix - 1) + arr[(j - 1) * Matrix + Matrix - 1]; //right 
         } 
  
-        cublasHandle_t handle; 
-        cublasCreate(&handle); 
  
-        while (err > TOL && iter < ITER_MAX) { 
+        while (err > accuracy && err > 0.000001 && iter < iterations) { 
             // Compute new values 
             err = 0; 
-            #pragma acc kernels loop independent reduction(max:err) 
-            for (int j = 1; j < M - 1; j++) { 
-                #pragma acc loop reduction(max:err) 
-                for (int i = 1; i < M - 1; i++) { 
-                    int index = j*M+i; 
-                    arrNew[index] = 0.25 * (arr[index + M] + arr[index - M] + 
-                                             arr[index - 1] + arr[index + 1]); 
-                    err = fmax(err, fabs(arrNew[index] - arr[index])); 
+#pragma acc kernels loop independent reduction(max:err) 
+            for (int j = 1; j < Matrix - 1; j++) { 
+#pragma acc loop reduction(max:err) 
+                for (int i = 1; i < Matrix - 1; i++) { 
+                    int index = j * Matrix + i; 
+                    array_new[index] = 0.25 * (arr[index + Matrix] + arr[index - Matrix] + 
+                        arr[index - 1] + arr[index + 1]); 
+                    err = fmax(err, fabs(array_new[index] - arr[index])); 
                 } 
             } 
             // Update values 
-            cublasDcopy(handle, M * M, arrNew, 1, arr, 1); 
+#pragma acc kernels loop independent 
+            for (int j = 1; j < Matrix - 1; j++) { 
+#pragma acc loop 
+                for (int i = 1; i < Matrix - 1; i++) { 
+                    int index = j * Matrix + i; 
+                    arr[index] = array_new[index]; 
+                } 
+            } 
  
             iter++; 
-             
+ 
             // Print progress 
             if (iter % 100 == 0) { 
-                printf("%d, %0.6lf\n", iter, err); 
+                //printf("%d, %0.6lf\n", iter, err); 
             } 
         } 
- 
-        cublasDestroy(handle); 
     } 
-     
+ 
     printf("Final result: %d, %0.6lf\n", iter, err); 
     // Free memory 
     free(arr); 
-    free(arrNew); 
+    free(array_new); 
+ 
+     
+    clock_t end1 = clock(); 
+    time_spent1 += (double)(end1 - begin1) / CLOCKS_PER_SEC; 
+    printf("%f\n", time_spent1); 
      
     return 0; 
 }
