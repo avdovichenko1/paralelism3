70397097b089150b136ba21da98c21f447e6a28b
avdovichenko1 <92294006+avdovichenko1@users.noreply.github.com>
2023-03-27 13:48:41 +0700

    Update main.c

--- main.c
+++ main.c
@@ -21,16 +21,13 @@ int main(int argc, char *argv[]) {
 
     double* arr_pred = (double*)calloc(raz * raz, sizeof(double));
     double* arr_new = (double*)calloc(raz * raz, sizeof(double));
-
-    double x1 = 10.0;
-    double x2 = 20.0;
-    double y1 = 20.0;
-    double y2 = 30.0;
+    
     arr_pred[0] = 10;
     arr_pred[raz] = 20;
     arr_pred[raz * (raz - 1) + 1] = 20;
     arr_pred[raz * raz] = 30;
     
+    int num_iter = 0;
     double error = 1 + max_toch;
     // Move data to device (accelerator)
 #pragma acc enter data create(arr_pred[0:raz*raz], arr_new[0:raz*raz]) copyin(raz, step1)
@@ -39,19 +36,18 @@ int main(int argc, char *argv[]) {
         // Initialize boundary conditions
 #pragma acc loop independent
         for (int i = 0; i < raz; i++) {
-            arr_new[i * raz] = arr_pred[i * raz] = x1 + i * step1;
-            arr_new[i] = arr_pred[i] = x1 + i * step1;
-            arr_new[(raz - 1) * raz + i] = arr_pred[(raz - 1) * raz + i] = y1 + i * step1;
-            arr_new[i * raz + (raz - 1)] = arr_pred[i * raz + (raz - 1)] = x2 + i * step1;
+            arr_new[i * raz] = arr_pred[i * raz] = 10 + i * step1;
+            arr_new[i] = arr_pred[i] = 10 + i * step1;
+            arr_new[(raz - 1) * raz + i] = arr_pred[(raz - 1) * raz + i] = 20 + i * step1;
+            arr_new[i * raz + (raz - 1)] = arr_pred[i * raz + (raz - 1)] = 20 + i * step1;
         }
     }
-
-    int itter = 0;
+    
     // Perform iterations until convergence
-    while (itter < 1000000 && error > 1e-6) {
-        itter++;
+    while (num_iter < 1000000 && error > 1e-6) {
+        num_iter++;
         // Every 100 iterations or the first iteration, calculate error
-        if (itter % 100 == 0 || itter == 1) {
+        if (num_iter % 100 == 0 || num_iter == 1) {
             // Perform Jacobi iteration on device
 #pragma acc data present(arr_pred[0:raz*raz], arr_new[0:raz*raz])
 #pragma acc kernels async(1)
@@ -96,13 +92,13 @@ int main(int argc, char *argv[]) {
         arr_new = arr_pred;
         arr_pred = dop;
 
-        if (itter % 100 == 0 || itter == 1)
+        if (num_iter % 100 == 0 || num_iter == 1)
 #pragma acc wait(1)
-            printf("%d %e\n", itter, error);
+            printf("%d %e\n", num_iter, error);
 
     }
 
-    printf("%d\n", itter);
+    printf("%d\n", num_iter);
     printf("%e", error);
     cublasDestroy(handle);
     return 0;
